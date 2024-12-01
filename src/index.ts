f5043eadba03f314caea9cf1a33a763349b25057
roll1226 <tomo_yasu_4724@icloud.com>
2024-12-01 11:03:32 +0900

    feat: post

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,20 @@ app.get("/entry/:date/:id", (c) => {
   return c.json({
     date,
     id,
+    ok: true,
   });
+});
+
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want see ${page} of ${id}`);
+});
+
+app.post("/posts", (c) => c.text("Created!", 201));
+app.delete("/posts/:id", (c) => {
+  return c.text(`${c.req.param("id")} is deleted!`);
 });
 
 const port = 3000;
