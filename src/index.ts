09a94ddadcf907d84ab0c079a1cdbdbdea2dbc04
roll1226 <tomo_yasu_4724@icloud.com>
2024-12-01 10:54:41 +0900

    feat: json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import { serve } from "@hono/node-server";
+import console from "console";
 import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono();
+
+app.use("*", prettyJSON());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
 app.get("/hello/:name", (c) => {
-  return c.text(`Hello ${c.req.param("name")}!`);
+  return c.json({ message: `Hello ${c.req.param("name")}!` });
+});
+
+app.get("/entry/:date/:id", (c) => {
+  const date = c.req.param("date");
+  const id = c.req.param("id");
+
+  return c.json({
+    date,
+    id,
+  });
 });
 
 const port = 3000;
